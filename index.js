4339a4aa035c163cca4f3e9e334116c7dd6b00fa
Alex Dauncey <84759985+Shylexx@users.noreply.github.com>
2022-11-23 20:37:02 +0000

    added unnecessary dependencies

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const c = console.log.bind(document);
+const log = require('simple-node-logger').createSimpleLogger();
 
+const console_init  = () => {
+  log.setLevel('trace');
+  log.log("Initializing Console Logger")
+  let cons = console.log.bind(document);
+  log.log("Console Logger Function initialized and bound");
+  return cons;
+}
+
+const c = console_init();
+ 
 module.exports = {
   c
 }
